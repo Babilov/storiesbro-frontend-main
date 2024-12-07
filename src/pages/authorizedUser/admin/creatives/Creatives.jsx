4889d2c3c19f9cf990658565ff4d5e7f224dc2b1
babilovv <egor-kun2017@mail.ru>
2024-12-07 20:12:18 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -1,11 +1,20 @@
 import { Box } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreativessBeforeEnter from "../../../../components/AuthorizedAdmin/creatives/creativesBefoteEnter/CreativessBeforeEnter";
 import CreativessBeforeEnter1 from "../../../../components/AuthorizedAdmin/creatives/creativesBefoteEnter/CreativessBeforeEnter1";
 import CreativesAfterEnter from "../../../../components/AuthorizedAdmin/creatives/creativesAfterEnter/CreativesAfterEnter";
 
 const Creatives = () => {
   const [authed, setAuthed] = useState(false);
+
+  useEffect(() => {
+    const isAuthed = localStorage.getItem("is_authed");
+    if (isAuthed === "true") {
+      setAuthed(true);
+    } else {
+      setAuthed(false);
+    }
+  }, []);
 
   return (
     <>
