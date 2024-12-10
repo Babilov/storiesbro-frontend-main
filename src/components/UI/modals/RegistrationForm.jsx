92872173eeb114886f98e9bc4e5e56dc40153d1f
babilovv <egor-kun2017@mail.ru>
2024-12-10 12:22:09 +0300

    1

--- src/components/UI/modals/RegistrationForm.jsx
+++ src/components/UI/modals/RegistrationForm.jsx
@@ -52,7 +52,9 @@ const RegistrationForm = ({
 
   const checkIsEmail = async (email) => {
     try {
-      const response = await axios.get(`${API_URL}check_email/${email}/`);
+      const response = await axios.get(
+        `https://storisbro.com/api/check_email/${email}/`,
+      );
       if (response.data.email) {
         setError(true);
         setErrorMessage("Пользователь с такой почтой уже зарегестрирован!");
@@ -101,7 +103,7 @@ const RegistrationForm = ({
 
     if (!checkSymbolsPassword(password)) {
       setErrorMessage(
-        "*В пароле должны быть только латинские и специальные символы"
+        "*В пароле должны быть только латинские и специальные символы",
       );
       setError(true);
       setIsEmailConfirm(false);
