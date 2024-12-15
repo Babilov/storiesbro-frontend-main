6d1f50004d0ea32975f264b39bb64a0c4fccb70e
babilovv <egor-kun2017@mail.ru>
2024-12-15 14:45:05 +0300

    1

--- src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/modals/AddPublicModal.jsx
+++ src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/modals/AddPublicModal.jsx
@@ -50,6 +50,7 @@ const AddPublicModal = ({ open, setOpen, publics }) => {
           );
           console.log("Данные успешно отправлены");
           logToBackend(JSON.stringify(setOpen));
+          window.location.reload();
         } catch (error) {
           console.error("Ошибка при отправке данных:", error);
         }
