cc793969ba90f411adaef4e37c820d35e5f2657f
babilovv <egor-kun2017@mail.ru>
2024-12-15 12:45:31 +0300

    1

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ function App() {
     };
 
     fetchAllPublics();
-  }, [userId]);
+  }, [publics]);
 
   useEffect(() => {
     // Функция для получения сообществ с бэкенда
@@ -123,24 +123,24 @@ function App() {
   }, [userId]);
 
   /*
-                                                                    useEffect(() => {
-                                                                      // Функция для получения креативов с бэкенда
-                                                                      const fetchCreativesAll = async () => {
-                                                                        try {
-                                                                          const response = await axios.get(
-                                                                            `${API_URL}api_creatives/own_all_creatives/${userId}`,
-                                                                          );
-                                                                          setCreatives(response.data.filter((item) => item.archive === false));
-                                                                          setArhive(response.data.filter((item) => item.archive === true));
-                                                                        } catch (error) {
-                                                                          console.error("Ошибка при загрузке креативов", error);
-                                                                        }
-                                                                      };
-                                        
-                                                                      // Вызов функции для загрузки сообществ при монтировании компонента
-                                                                      fetchCreativesAll();
-                                                                    }, [userId]); // Пустой массив зависимостей означает, что эффект будет выполнен только один раз при монтировании
-                                                                  */
+                                                                      useEffect(() => {
+                                                                        // Функция для получения креативов с бэкенда
+                                                                        const fetchCreativesAll = async () => {
+                                                                          try {
+                                                                            const response = await axios.get(
+                                                                              `${API_URL}api_creatives/own_all_creatives/${userId}`,
+                                                                            );
+                                                                            setCreatives(response.data.filter((item) => item.archive === false));
+                                                                            setArhive(response.data.filter((item) => item.archive === true));
+                                                                          } catch (error) {
+                                                                            console.error("Ошибка при загрузке креативов", error);
+                                                                          }
+                                                                        };
+                                          
+                                                                        // Вызов функции для загрузки сообществ при монтировании компонента
+                                                                        fetchCreativesAll();
+                                                                      }, [userId]); // Пустой массив зависимостей означает, что эффект будет выполнен только один раз при монтировании
+                                                                    */
   // автоматическая очистка кэша
 
   if (localStorage.getItem("token")) {
