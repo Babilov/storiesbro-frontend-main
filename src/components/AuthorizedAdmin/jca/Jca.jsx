a3fba1837f6d0ddb62829bd4f234b65dce47f113
babilovv <egor-kun2017@mail.ru>
2024-12-17 14:11:13 +0300

    tooltips

--- src/components/AuthorizedAdmin/jca/Jca.jsx
+++ src/components/AuthorizedAdmin/jca/Jca.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AuthorizedAdminCarusel from "../../UI/myCarousel/AuthorizedAdminCarusel";
 
 import jca from "./images/jca.png";
-import {Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import firstVideo from "./images/IMG_8426.mp4";
 import secondVideo from "./images/IMG_8425.mp4";
 import thirdVideo from "./images/IMG_8423.mp4";
+import { useLocation } from "react-router-dom";
 
 const Jca = () => {
-    return (
-        <Box sx={{mt: 5}}>
-            <Typography className="description">
-                {" "}
-                <Typography component="span" className="orange bold">
-                    Стандартные ЖЦА{" "}
-                </Typography>
-                - это универсальные развлекательные истории для женской аудитории
-            </Typography>
-            <Typography className="title" sx={{mt: 4}}>
-                Примеры стандартных ЖЦА
-            </Typography>
-            <AuthorizedAdminCarusel firstVideo={firstVideo} secondVideo={secondVideo} thirdVideo={thirdVideo}/>
-        </Box>
-    );
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [hash]);
+
+  return (
+    <Box sx={{ mt: 5 }} id="jca">
+      <Typography className="description">
+        {" "}
+        <Typography component="span" className="orange bold">
+          Стандартные ЖЦА{" "}
+        </Typography>
+        - это универсальные развлекательные истории для женской аудитории
+      </Typography>
+      <Typography className="title" sx={{ mt: 4 }}>
+        Примеры стандартных ЖЦА
+      </Typography>
+      <AuthorizedAdminCarusel
+        firstVideo={firstVideo}
+        secondVideo={secondVideo}
+        thirdVideo={thirdVideo}
+      />
+    </Box>
+  );
 };
 
 export default Jca;
--- src/components/AuthorizedAdmin/vca/Vca.jsx
+++ src/components/AuthorizedAdmin/vca/Vca.jsx
@@ -1,28 +1,43 @@
-import React from "react";
-import {Box, Typography} from "@mui/material";
+import React, { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
 
 import vca from "./images/vca.png";
 import AuthorizedAdminCarusel from "../../UI/myCarousel/AuthorizedAdminCarusel";
 import firstVideo from "./images/IMG_8429.mp4";
 import secondVideo from "./images/IMG_8428.mp4";
 import thirdVideo from "./images/IMG_8427.mp4";
+import { useLocation } from "react-router-dom";
 
 const Vca = () => {
-    return (
-        <Box sx={{mt: 5}}>
-            <Typography className="description">
-                <Typography component="span" className="orange bold">
-                    Стандартные СЦА
-                </Typography>{" "}
-                - это универсальные развлекательные истории для мужской и женской
-                аудитории старше 25 лет.
-            </Typography>
-            <Typography className="title" sx={{mt: 4}}>
-                Примеры стандартных СЦА
-            </Typography>
-            <AuthorizedAdminCarusel firstVideo={firstVideo} secondVideo={secondVideo} thirdVideo={thirdVideo}/>
-        </Box>
-    );
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [hash]);
+  return (
+    <Box sx={{ mt: 5 }} id="vca">
+      <Typography className="description">
+        <Typography component="span" className="orange bold">
+          Стандартные СЦА
+        </Typography>{" "}
+        - это универсальные развлекательные истории для мужской и женской
+        аудитории старше 25 лет.
+      </Typography>
+      <Typography className="title" sx={{ mt: 4 }}>
+        Примеры стандартных СЦА
+      </Typography>
+      <AuthorizedAdminCarusel
+        firstVideo={firstVideo}
+        secondVideo={secondVideo}
+        thirdVideo={thirdVideo}
+      />
+    </Box>
+  );
 };
 
 export default Vca;
