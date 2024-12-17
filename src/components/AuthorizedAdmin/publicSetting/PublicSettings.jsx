016207ee7c78d9613b2ca42172d961d08891c77e
babilovv <egor-kun2017@mail.ru>
2024-12-17 11:59:31 +0300

    1

--- src/components/AuthorizedAdmin/publicSetting/PublicSettings.jsx
+++ src/components/AuthorizedAdmin/publicSetting/PublicSettings.jsx
@@ -32,174 +32,177 @@ const PublicSettings = () => {
 
   return (
     <Grid container className="grid">
-      {console.log(publicObj)}
-      <Grid item xs={12}>
-        <Typography className="title">Настройки сообщества</Typography>
-      </Grid>
+      {publicObj !== undefined ? (
+        <>
+          <Grid item xs={12}>
+            <Typography className="title">Настройки сообщества</Typography>
+          </Grid>
 
-      <Grid
-        item
-        xs={12}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Box
-          component="img"
-          alt="img"
-          src={publicObj?.group?.image || ""}
-          sx={{ width: "15%" }}
-        />
-        <Typography sx={{ fontSize: "24px", fontWeight: 600, mt: 4, mb: 4 }}>
-          {/* {publicObj["name"]} */}
-          {/* {publics[0]['name']} */}
-          {publicObj?.group?.name || ""}
-        </Typography>
-      </Grid>
-
-      <Grid
-        item
-        md={9}
-        container
-        className="grayBorder"
-        sx={{ m: "0 auto", p: "16px 16px 0 16px" }}
-      >
-        <Grid item xs={12} sx={{ borderBottom: "1px solid #CDCDCD" }}>
-          <Typography
+          <Grid
+            item
+            xs={12}
             sx={{
-              fontSize: { sm: "18px", xs: "12px" },
-              fontWeight: 600,
-              textAlign: "center",
-              mb: 2,
-              cursor: "pointer",
-            }}
-          >
-            Контент-видео
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          md={3}
-          xs={12}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Box
-            sx={{
-              borderRight: { md: "1px solid black" },
               display: "flex",
-              justifyContent: "space-around",
+              flexDirection: "column",
+              justifyContent: "center",
               alignItems: "center",
-              mt: "10px",
-              mb: "10px",
             }}
           >
-            <Typography
-              sx={{
-                fontSize: { sm: "18px", xs: "14px" },
-                fontWeight: 500,
-                textAlign: { md: "center", xs: "left" },
-                cursor: "pointer",
-                color: "#E37E31",
-                ":hover": { color: "#E37E31" },
-              }}
-            >
-              Стандартные МЦА
-            </Typography>
             <Box
               component="img"
-              src={question}
-              alt="question"
-              sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
+              alt="img"
+              src={publicObj?.group?.image || ""}
+              sx={{ width: "15%" }}
             />
-          </Box>
-        </Grid>
-
-        <Grid
-          item
-          md={3}
-          xs={12}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-around",
-              alignItems: "center",
-              borderRight: { md: "1px solid black" },
-              mt: "10px",
-              mb: "10px",
-            }}
-          >
             <Typography
-              sx={{
-                fontSize: { sm: "18px", xs: "14px" },
-                fontWeight: 500,
-                textAlign: { md: "center", xs: "left" },
-
-                cursor: "pointer",
-                ":hover": { color: "#E37E31" },
-              }}
+              sx={{ fontSize: "24px", fontWeight: 600, mt: 4, mb: 4 }}
             >
-              Стандартные ЖЦА
+              {/* {publicObj["name"]} */}
+              {/* {publics[0]['name']} */}
+              {publicObj?.group?.name || ""}
             </Typography>
-            <Box
-              component="img"
-              src={question}
-              alt="question"
-              sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
-            />
-          </Box>
-        </Grid>
+          </Grid>
 
-        <Grid
-          item
-          md={3}
-          xs={12}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-around",
-              alignItems: "center",
-              borderRight: { md: "1px solid black" },
-              mt: "10px",
-              mb: "10px",
-            }}
+          <Grid
+            item
+            md={9}
+            container
+            className="grayBorder"
+            sx={{ m: "0 auto", p: "16px 16px 0 16px" }}
           >
-            <Typography
-              sx={{
-                fontSize: { sm: "18px", xs: "14px" },
-                fontWeight: 500,
-                textAlign: { md: "center", xs: "left" },
-
-                cursor: "pointer",
-                ":hover": { color: "#E37E31" },
-              }}
-            >
-              Стандартные ВЦА
-            </Typography>
-            <Tooltip
-              title={
-                <>
-                  <Typography>Взрослая целевая аудитория</Typography>
-                  <Link> Подробнее </Link>
-                </>
-              }
+            <Grid item xs={12} sx={{ borderBottom: "1px solid #CDCDCD" }}>
+              <Typography
+                sx={{
+                  fontSize: { sm: "18px", xs: "12px" },
+                  fontWeight: 600,
+                  textAlign: "center",
+                  mb: 2,
+                  cursor: "pointer",
+                }}
+              >
+                Контент-видео
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sx={{ display: "flex", alignItems: "center" }}
             >
               <Box
-                component="img"
-                src={question}
-                alt="question"
-                sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
-              />
-            </Tooltip>
-          </Box>
-        </Grid>
-        {/*
+                sx={{
+                  borderRight: { md: "1px solid black" },
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  mt: "10px",
+                  mb: "10px",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: { sm: "18px", xs: "14px" },
+                    fontWeight: 500,
+                    textAlign: { md: "center", xs: "left" },
+                    cursor: "pointer",
+                    color: "#E37E31",
+                    ":hover": { color: "#E37E31" },
+                  }}
+                >
+                  Стандартные МЦА
+                </Typography>
+                <Box
+                  component="img"
+                  src={question}
+                  alt="question"
+                  sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
+                />
+              </Box>
+            </Grid>
+
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  borderRight: { md: "1px solid black" },
+                  mt: "10px",
+                  mb: "10px",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: { sm: "18px", xs: "14px" },
+                    fontWeight: 500,
+                    textAlign: { md: "center", xs: "left" },
+
+                    cursor: "pointer",
+                    ":hover": { color: "#E37E31" },
+                  }}
+                >
+                  Стандартные ЖЦА
+                </Typography>
+                <Box
+                  component="img"
+                  src={question}
+                  alt="question"
+                  sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
+                />
+              </Box>
+            </Grid>
+
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  borderRight: { md: "1px solid black" },
+                  mt: "10px",
+                  mb: "10px",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: { sm: "18px", xs: "14px" },
+                    fontWeight: 500,
+                    textAlign: { md: "center", xs: "left" },
+
+                    cursor: "pointer",
+                    ":hover": { color: "#E37E31" },
+                  }}
+                >
+                  Стандартные ВЦА
+                </Typography>
+                <Tooltip
+                  title={
+                    <>
+                      <Typography>Взрослая целевая аудитория</Typography>
+                      <Link> Подробнее </Link>
+                    </>
+                  }
+                >
+                  <Box
+                    component="img"
+                    src={question}
+                    alt="question"
+                    sx={{ ml: "5px", mr: "20px", cursor: "pointer" }}
+                  />
+                </Tooltip>
+              </Box>
+            </Grid>
+            {/*
         <Grid item md={2.4} xs={12}>
           <Typography
             sx={{
@@ -215,87 +218,91 @@ const PublicSettings = () => {
           </Typography>
         </Grid>
 */}
-        <Grid
-          item
-          md={3}
-          xs={12}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-around",
-              alignItems: "center",
-              mt: "10px",
-              mb: "10px",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: { sm: "18px", xs: "14px" },
-                fontWeight: { sm: 400, xs: 500 },
-                textAlign: { md: "center", xs: "left" },
-                cursor: "pointer",
-                ":hover": { color: "#E37E31" },
-              }}
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sx={{ display: "flex", alignItems: "center" }}
             >
-              Не постить
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  mt: "10px",
+                  mb: "10px",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: { sm: "18px", xs: "14px" },
+                    fontWeight: { sm: 400, xs: 500 },
+                    textAlign: { md: "center", xs: "left" },
+                    cursor: "pointer",
+                    ":hover": { color: "#E37E31" },
+                  }}
+                >
+                  Не постить
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
 
-      <Grid
-        item
-        md={4}
-        container
-        className="grayBorder"
-        sx={{ m: "40px auto", p: 2 }}
-      >
-        <Grid item xs={12} sx={{ borderBottom: "1px solid #CDCDCD" }}>
-          <Typography
-            sx={{
-              fontSize: { sm: "18px", xs: "12px" },
-              fontWeight: 600,
-              textAlign: "center",
-              mb: 2,
-            }}
+          <Grid
+            item
+            md={4}
+            container
+            className="grayBorder"
+            sx={{ m: "40px auto", p: 2 }}
           >
-            Состояние
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography
-            sx={{
-              borderRight: "1px solid black",
-              fontSize: { sm: "18px", xs: "14px" },
-              fontWeight: 500,
-              textAlign: "center",
-              mt: 2,
-              cursor: "pointer",
-              color: state ? "#D25D48" : "black",
-            }}
-            onClick={() => setState(true)}
-          >
-            Включить
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography
-            sx={{
-              fontSize: { sm: "18px", xs: "14px" },
-              fontWeight: 500,
-              textAlign: "center",
-              mt: 2,
-              cursor: "pointer",
-              color: !state ? "#D25D48" : "black",
-            }}
-            onClick={() => setState(false)}
-          >
-            Отключено
-          </Typography>
-        </Grid>
-      </Grid>
+            <Grid item xs={12} sx={{ borderBottom: "1px solid #CDCDCD" }}>
+              <Typography
+                sx={{
+                  fontSize: { sm: "18px", xs: "12px" },
+                  fontWeight: 600,
+                  textAlign: "center",
+                  mb: 2,
+                }}
+              >
+                Состояние
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                sx={{
+                  borderRight: "1px solid black",
+                  fontSize: { sm: "18px", xs: "14px" },
+                  fontWeight: 500,
+                  textAlign: "center",
+                  mt: 2,
+                  cursor: "pointer",
+                  color: state ? "#D25D48" : "black",
+                }}
+                onClick={() => setState(true)}
+              >
+                Включить
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                sx={{
+                  fontSize: { sm: "18px", xs: "14px" },
+                  fontWeight: 500,
+                  textAlign: "center",
+                  mt: 2,
+                  cursor: "pointer",
+                  color: !state ? "#D25D48" : "black",
+                }}
+                onClick={() => setState(false)}
+              >
+                Отключено
+              </Typography>
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <Typography>Загрузка...</Typography>
+      )}
     </Grid>
   );
 };
