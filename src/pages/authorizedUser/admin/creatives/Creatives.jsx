1b1f1f5d582d6747e0678daf3cc76a43a76c474a
babilovv <egor-kun2017@mail.ru>
2024-12-21 16:43:49 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -2,31 +2,32 @@ import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import CreativessBeforeEnter from "../../../../components/AuthorizedAdmin/creatives/creativesBefoteEnter/CreativessBeforeEnter";
 import CreativesAfterEnter from "../../../../components/AuthorizedAdmin/creatives/creativesAfterEnter/CreativesAfterEnter";
+import axios from "axios";
+import logToBackend from "../../../../utils/logs";
 
 const Creatives = () => {
   const [authed, setAuthed] = useState(false);
+  /*
+        useEffect(() => {
+          const isAuthed = localStorage.getItem("is_authed");
+          if (isAuthed === "true") {
+            setAuthed(true);
+          } else {
+            setAuthed(false);
+          }
+        }, []);
+      */
 
   useEffect(() => {
-    const isAuthed = localStorage.getItem("is_authed");
-    if (isAuthed === "true") {
-      setAuthed(true);
-    } else {
-      setAuthed(false);
-    }
+    const fetchAuthed = async () => {
+      const isAuthed = await axios.get(
+        "https://storisbro.com/api/auth-status/",
+      );
+      logToBackend(`Front Authed: ${isAuthed}`);
+      setAuthed(isAuthed);
+    };
+    fetchAuthed();
   }, []);
-
-  /*
-      useEffect(() => {
-        const fetchAuthed = async () => {
-          const isAuthed = await axios.get(
-            "https://storisbro.com/api/auth-status/",
-          );
-          logToBackend(`Front Authed: ${isAuthed}`);
-          setAuthed(isAuthed);
-        };
-        fetchAuthed();
-      }, []);
-      */
 
   return (
     <>
