bea6d2ab482d14f2ecbaef2d619f7628738d278b
babilovv <egor-kun2017@mail.ru>
2024-12-25 14:34:08 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -8,22 +8,21 @@ import logToBackend from "../../../../utils/logs";
 const Creatives = () => {
   const [authed, setAuthed] = useState(false);
   /*
-          useEffect(() => {
-            const isAuthed = localStorage.getItem("is_authed");
-            if (isAuthed === "true") {
-              setAuthed(true);
-            } else {
-              setAuthed(false);
-            }
-          }, []);
-        */
+            useEffect(() => {
+              const isAuthed = localStorage.getItem("is_authed");
+              if (isAuthed === "true") {
+                setAuthed(true);
+              } else {
+                setAuthed(false);
+              }
+            }, []);
+          */
 
   useEffect(() => {
     const fetchAuthed = async () => {
       const userId = localStorage.getItem("id");
       const isAuthed = await axios.get(
-        "https://storisbro.com/api/auth-status/",
-        { data: { user_id: userId } },
+        `https://storisbro.com/api/auth-status/?user_id=${userId}`,
       );
       logToBackend(`Front Authed: ${isAuthed}`);
       setAuthed(isAuthed);
