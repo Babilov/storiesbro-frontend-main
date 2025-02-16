c8f2fac8ab3b17315a55e8539ca007c4356a75bf
babilovv <egor-kun2017@mail.ru>
2025-02-16 22:37:23 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -12,15 +12,30 @@ const Creatives = () => {
   logToBackend(`LOCALSTORAGE IS_VK_AUTH: ${is_vk_authed}`);
 
   useEffect(() => {
-    logToBackend("МЫ В ЮЗ ЭФФЕКТЕ");
-    if (is_vk_authed === "true") {
-      logToBackend("ОТРАБОТАЛО TRUE");
-      setAuthed(true);
-    } else {
-      logToBackend("ОТРАБОТАЛО FALSE");
-      setAuthed(false);
-    }
-    logToBackend(`AUTHED ===== ${authed}`);
+    const fetchVkAuth = async () => {
+      const token = localStorage.getItem("access_token");
+      logToBackend("МЫ В ЮЗ ЭФФЕКТЕ");
+      if (is_vk_authed === null) {
+        const new_vk_auth = await axios.get(
+          `https://storisbro.com/api/auth-status/`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, // Токен в заголовке
+            },
+          },
+        );
+        setAuthed(new_vk_auth["data"]["authenticated"]);
+      }
+      if (is_vk_authed === "true") {
+        logToBackend("ОТРАБОТАЛО TRUE");
+        setAuthed(true);
+      } else {
+        logToBackend("ОТРАБОТАЛО FALSE");
+        setAuthed(false);
+      }
+      logToBackend(`AUTHED ===== ${authed}`);
+    };
+    fetchVkAuth();
   }, [is_vk_authed]);
 
   return (
