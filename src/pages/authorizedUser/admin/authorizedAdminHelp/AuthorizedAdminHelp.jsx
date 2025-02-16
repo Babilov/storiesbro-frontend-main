3862afb7c0e2b73e6f14b88f1649d2b469a4dcd5
babilovv <egor-kun2017@mail.ru>
2025-02-17 00:05:58 +0300

    1

--- src/pages/authorizedUser/admin/authorizedAdminHelp/AuthorizedAdminHelp.jsx
+++ src/pages/authorizedUser/admin/authorizedAdminHelp/AuthorizedAdminHelp.jsx
@@ -1,5 +1,5 @@
-import {Box, Container, Grid} from "@mui/material";
-import React, {useEffect} from "react";
+import { Box, Container, Grid } from "@mui/material";
+import React, { useEffect } from "react";
 import HelpSteps from "../../../../components/AuthorizedAdmin/helpSteps/HelpSteps";
 import PublicRequirements from "../../../../components/AuthorizedAdmin/publicRequirements/PublicRequirements";
 import ClosingBlock from "../../../../components/AuthorizedAdmin/closingBlock/ClosingBlock";
@@ -8,38 +8,72 @@ import OwnVideosInstruction from "../../../../components/AuthorizedAdmin/ownVide
 import HowToIncrease from "../../../../components/AuthorizedAdmin/howToIncrease/HowToIncrease";
 import AuthorizedAdminMenu from "../../../../components/AuthorizedAdmin/menu/AuthorizedAdminMenu";
 import AuthorizedUserHeader from "../../../../components/authorizedUser/authorizedUserHeader/AuthorizedUserHeader";
+import logToBackend from "../../../../utils/logs";
+import axios from "axios";
 
 const AuthorizedAdminHelp = () => {
-    useEffect(() => {
-        document.body.classList.add("no-scrollbar");
+  useEffect(() => {
+    document.body.classList.add("no-scrollbar");
 
-        return () => {
-            document.body.classList.remove("no-scrollbar");
-        };
-    }, []);
-    return (
-        <Container sx={{msOverflowStyle: "none", scrollbarWidth: "none", overflow: "hidden",}}>
-            <AuthorizedUserHeader ismainpage={true}/>
-            <Grid container>
-                <Grid item lg={2} xs={0}>
-                    <AuthorizedAdminMenu/>
-                </Grid>
-                <Grid item md={10}>
+    return () => {
+      document.body.classList.remove("no-scrollbar");
+    };
+  }, []);
 
-                    <HelpSteps/>
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    const fetchVkAuth = async () => {
+      try {
+        const response = await axios.get(
+          "https://storisbro.com/api/auth-status/",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          },
+        );
 
-                </Grid>
-            </Grid>
-            <Box sx={{width: "100%"}}>
-                <PublicRequirements/>
-                <ClosingBlock/>
-                <ContentVideos/>
-                <OwnVideosInstruction/>
-                <HowToIncrease/>
-            </Box>
+        const authenticated = response["data"]["authenticated"];
 
-        </Container>
-    );
+        localStorage.setItem("is_vk_authed", JSON.stringify(authenticated));
+
+        await logToBackend(
+          `Результат запроса (ADMINHELP): ${JSON.stringify(response.data)}`,
+        );
+      } catch (error) {
+        await logToBackend(
+          `Ошибка при проверке авторизации (ADMINHELP): ${error.message}`,
+          "ERROR",
+        );
+      }
+    };
+    fetchVkAuth();
+  }, []);
+
+  return (
+    <Container
+      sx={{
+        msOverflowStyle: "none",
+        scrollbarWidth: "none",
+        overflow: "hidden",
+      }}
+    >
+      <AuthorizedUserHeader ismainpage={true} />
+      <Grid container>
+        <Grid item lg={2} xs={0}>
+          <AuthorizedAdminMenu />
+        </Grid>
+        <Grid item md={10}>
+          <HelpSteps />
+        </Grid>
+      </Grid>
+      <Box sx={{ width: "100%" }}>
+        <PublicRequirements />
+        <ClosingBlock />
+        <ContentVideos />
+        <OwnVideosInstruction />
+        <HowToIncrease />
+      </Box>
+    </Container>
+  );
 };
 
 export default AuthorizedAdminHelp;
