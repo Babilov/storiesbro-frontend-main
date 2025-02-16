b597d0cba063b5a136e2bd6e12875935b8b1358d
babilovv <egor-kun2017@mail.ru>
2025-02-16 21:56:25 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -7,52 +7,50 @@ import logToBackend from "../../../../utils/logs";
 
 const Creatives = () => {
   const [authed, setAuthed] = useState(false);
+  const is_vk_authed = localStorage.getItem("is_vk_authed");
   /*
-                                                                useEffect(() => {
-                                                                  const isAuthed = localStorage.getItem("is_authed");
-                                                                  if (isAuthed === "true") {
-                                                                    setAuthed(true);
-                                                                  } else {
-                                                                    setAuthed(false);
-                                                                  }
-                                                                }, []);
-                                                                */
+                                                                      useEffect(() => {
+                                                                        const isAuthed = localStorage.getItem("is_authed");
+                                                                        if (isAuthed === "true") {
+                                                                          setAuthed(true);
+                                                                        } else {
+                                                                          setAuthed(false);
+                                                                        }
+                                                                      }, []);
+                                                                      */
   /*
-                                useEffect(() => {
-                                  const fetchAuthed = async () => {
-                                    const userId = localStorage.getItem("id");
-                                    const token = localStorage.getItem("access_token");
-                                    const isAuthed = await axios.get(
-                                      `https://storisbro.com/api/auth-status/`,
-                                      {
-                                        headers: {
-                                          Authorization: `Bearer ${token}`, // Токен в заголовке
-                                        },
-                                      },
-                                    );
-                                    logToBackend(`Front Authed: ${JSON.stringify(isAuthed)}`);
-                                    // console.log(isAuthed.data.authenticated);
-                                    setAuthed(isAuthed.data.authenticated);
-                                  };
-                                  fetchAuthed();
-                                }, []);
-                                */
+                                      useEffect(() => {
+                                        const fetchAuthed = async () => {
+                                          const userId = localStorage.getItem("id");
+                                          const token = localStorage.getItem("access_token");
+                                          const isAuthed = await axios.get(
+                                            `https://storisbro.com/api/auth-status/`,
+                                            {
+                                              headers: {
+                                                Authorization: `Bearer ${token}`, // Токен в заголовке
+                                              },
+                                            },
+                                          );
+                                          logToBackend(`Front Authed: ${JSON.stringify(isAuthed)}`);
+                                          // console.log(isAuthed.data.authenticated);
+                                          setAuthed(isAuthed.data.authenticated);
+                                        };
+                                        fetchAuthed();
+                                      }, []);
+                                      */
 
   useEffect(() => {
-    const is_vk_authed = localStorage.getItem("is_vk_authed");
     if (is_vk_authed === "true") {
       setAuthed(true);
     } else {
       setAuthed(false);
     }
-  }, []);
+    logToBackend(authed);
+  }, [is_vk_authed]);
 
   return (
     <>
-      {logToBackend(`ABOBA1: ${localStorage.getItem("is_vk_authed")}`)}
-      {logToBackend(
-        `ABOBA: ${localStorage.getItem("is_vk_authed") === "false"}`,
-      )}
+      {logToBackend(`ABOBA: ${localStorage.getItem("is_vk_authed")}`)}
       {!authed ? (
         <CreativessBeforeEnter setAthed={setAuthed} />
       ) : (
