1fbaccd1b97e4991ff0e48de1ca475d4fcc6ac5c
babilovv <egor-kun2017@mail.ru>
2025-02-25 15:37:52 +0300

    1

--- src/pages/authorizedUser/admin/creatives/Creatives.jsx
+++ src/pages/authorizedUser/admin/creatives/Creatives.jsx
@@ -8,66 +8,56 @@ import { refreshToken } from "../../../../api/token";
 
 const Creatives = () => {
   const [authedVk, setAuthedVk] = useState(false);
+  useEffect(() => {
+    const refresh = async () => {
+      await refreshToken();
+    };
+    refresh().then(() => {
+      console.log("refreshed!!!!");
+    });
+  }, []);
 
-  const refresh = async () => {
-    await refreshToken();
+  const token = localStorage.getItem("access_token");
+
+  const ws = new WebSocket(
+    `wss://storisbro.com/ws/auth_status/?token=${token}`,
+  );
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log("Auth status changed:", data["authenticated"]);
+    setAuthedVk(data["authenticated"]);
+    logToBackend(`Auth status changed: ${data["authenticated"]}`);
   };
 
-  refresh().then(() => {
-    console.log("refreshed!!!!");
-    const token = localStorage.getItem("access_token");
-
-    const ws = new WebSocket(
-      `wss://storisbro.com/ws/auth_status/?token=${token}`,
-    );
-    ws.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log("Auth status changed:", data["authenticated"]);
-      setAuthedVk(data["authenticated"]);
-      logToBackend(`Auth status changed: ${data["authenticated"]}`);
-    };
-  });
-
-  let authed = localStorage.getItem("is_vk_authed");
-  useEffect(() => {
-    const fetchVkAuth = async () => {
-      // await logToBackend(`VK Auth статус из localStorage: ${authed}`);
-
-      if (authed === null || authed === "null") {
-        try {
-          await refreshToken();
-          const new_token = localStorage.getItem("access_token");
-
-          const response = await axios.get(
-            "https://storisbro.com/api/auth-status/",
-            {
-              headers: { Authorization: `Bearer ${new_token}` },
-            },
-          );
-
-          const authenticated = response["data"]["authenticated"];
-
-          localStorage.setItem("is_vk_authed", JSON.stringify(authenticated));
-          /*
-                                                                                                                                            await logToBackend(
-                                                                                                                                              `Результат запроса: ${JSON.stringify(response.data)}`,
-                                                                                                                                            );
-                                                  
-                                                                                                                                   */
-        } catch (error) {
-          /*
-                                                                                                                                  await logToBackend(
-                                                                                                                                    `Ошибка при проверке авторизации: ${error.message}`,
-                                                                                                                                    "ERROR",
-                                                                                                                                  );
-                                                                                                                                   */
-          console.log(error);
-        }
-      }
-    };
-
-    fetchVkAuth();
-  }, [authed]);
+  // let authed = localStorage.getItem("is_vk_authed");
+  /*useEffect(() => {
+        const fetchVkAuth = async () => {
+          // await logToBackend(`VK Auth статус из localStorage: ${authed}`);
+    
+          if (authed === null || authed === "null") {
+            try {
+              await refreshToken();
+              const new_token = localStorage.getItem("access_token");
+    
+              const response = await axios.get(
+                "https://storisbro.com/api/auth-status/",
+                {
+                  headers: { Authorization: `Bearer ${new_token}` },
+                },
+              );
+    
+              const authenticated = response["data"]["authenticated"];
+    
+              localStorage.setItem("is_vk_authed", JSON.stringify(authenticated));
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        };
+    
+        fetchVkAuth();
+      }, [authed]);
+      */
 
   return <>{!authedVk ? <CreativessBeforeEnter /> : <CreativesAfterEnter />}</>;
 };
