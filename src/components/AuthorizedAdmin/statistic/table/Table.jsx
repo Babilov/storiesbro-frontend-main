117a33fdd1def49b4918a4c3bbc3b515556422f8
babilovv <egor-kun2017@mail.ru>
2025-05-27 08:17:06 +0300

    1

--- src/components/AuthorizedAdmin/statistic/table/Table.jsx
+++ src/components/AuthorizedAdmin/statistic/table/Table.jsx
@@ -61,85 +61,95 @@ const Table = ({ statistic, open, groupInfo, startDate, endDate }) => {
           </Typography>
         </Grid>
         {statistic.map((statisticItem, index) => {
-          logToBackend(
-            `startDate: ${startDate}, period_from: ${statisticItem["period_from"]} endDate: ${endDate}, period_to: ${statisticItem["period_to"]}
-            ВЫРАЖЕНИЕ ${startDate <= statisticItem["period_from"]} ВЫРАЖЕНИЕ 2 ${endDate >= statisticItem["period_to"]} ВЫРАЖЕНИЕ 3 ${
-              startDate <= statisticItem["period_from"] &&
-              endDate >= statisticItem["period_to"]
-            }`
-          );
-          startDate <= statisticItem["period_from"] &&
-            endDate >= statisticItem["period_to"] && (
-              <Grid container key={index}>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "left",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {groupInfo["name"]}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "center",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {dayjs
-                      .unix(statisticItem["period_from"])
-                      .format("DD.MM.YYYY")}
-                    -
-                    {dayjs
-                      .unix(statisticItem["period_to"])
-                      .format("DD.MM.YYYY")}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "center",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {statisticItem["visitors"]["views"]}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "right",
-                      fontSize: { md: "18px", xs: "12px" },
-                      ml: 1,
-                      fontWeight: 400,
-                    }}
-                  >
-                    {0}
-                  </Typography>
-                </Grid>
-              </Grid>
-            );
+          {
+            statistic.map((statisticItem, index) => {
+              logToBackend(
+                `startDate: ${startDate}, period_from: ${statisticItem["period_from"]} endDate: ${endDate}, period_to: ${statisticItem["period_to"]}
+    ВЫРАЖЕНИЕ 1 ${startDate <= statisticItem["period_from"]} ВЫРАЖЕНИЕ 2 ${endDate >= statisticItem["period_to"]} ВЫРАЖЕНИЕ 3 ${
+      startDate <= statisticItem["period_from"] &&
+      endDate >= statisticItem["period_to"]
+    }`
+              );
+
+              if (
+                startDate <= statisticItem["period_from"] &&
+                endDate >= statisticItem["period_to"]
+              ) {
+                return (
+                  <Grid container key={index}>
+                    <Grid
+                      item
+                      xs={3}
+                      sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+                    >
+                      <Typography
+                        sx={{
+                          textAlign: "left",
+                          fontSize: { md: "18px", xs: "12px" },
+                          fontWeight: 400,
+                        }}
+                      >
+                        {groupInfo["name"]}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+                    >
+                      <Typography
+                        sx={{
+                          textAlign: "center",
+                          fontSize: { md: "18px", xs: "12px" },
+                          fontWeight: 400,
+                        }}
+                      >
+                        {dayjs
+                          .unix(statisticItem["period_from"])
+                          .format("DD.MM.YYYY")}
+                        -
+                        {dayjs
+                          .unix(statisticItem["period_to"])
+                          .format("DD.MM.YYYY")}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+                    >
+                      <Typography
+                        sx={{
+                          textAlign: "center",
+                          fontSize: { md: "18px", xs: "12px" },
+                          fontWeight: 400,
+                        }}
+                      >
+                        {statisticItem["visitors"]["views"]}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+                    >
+                      <Typography
+                        sx={{
+                          textAlign: "right",
+                          fontSize: { md: "18px", xs: "12px" },
+                          ml: 1,
+                          fontWeight: 400,
+                        }}
+                      >
+                        {0}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                );
+              }
+              return null; // или можно просто ничего не возвращать
+            });
+          }
         })}
       </Grid>
     )
