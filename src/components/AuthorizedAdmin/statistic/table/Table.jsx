dde5fd527f02a3aa4d6d2d5dde31b0030399b5dd
babilovv <egor-kun2017@mail.ru>
2025-05-30 10:06:01 +0300

    1

--- src/components/AuthorizedAdmin/statistic/table/Table.jsx
+++ src/components/AuthorizedAdmin/statistic/table/Table.jsx
@@ -60,92 +60,72 @@ const Table = ({ statistic, open, groupInfo, startDate, endDate }) => {
             Заработано
           </Typography>
         </Grid>
-        {statistic.map((statisticItem, index) => {
-          logToBackend(
-            `startDate: ${startDate}, period_from: ${statisticItem["period_from"]} endDate: ${endDate}, period_to: ${statisticItem["period_to"]}
-    ВЫРАЖЕНИЕ 1 ${startDate <= statisticItem["period_from"]} ВЫРАЖЕНИЕ 2 ${endDate >= statisticItem["period_to"]} ВЫРАЖЕНИЕ 3 ${
-      startDate <= statisticItem["period_from"] &&
-      endDate >= statisticItem["period_to"]
-    }`
-          );
-
-          if (
-            startDate <= statisticItem["period_from"] &&
-            endDate >= statisticItem["period_to"]
-          ) {
-            return (
-              <Grid container key={index}>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+        {Object.entries(statistic).map(([date, views]) => {
+          return (
+            <Grid container key={date}>
+              <Grid
+                item
+                xs={3}
+                sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+              >
+                <Typography
+                  sx={{
+                    textAlign: "left",
+                    fontSize: { md: "18px", xs: "12px" },
+                    fontWeight: 400,
+                  }}
                 >
-                  <Typography
-                    sx={{
-                      textAlign: "left",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {groupInfo["name"]}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={4}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "center",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {dayjs
-                      .unix(statisticItem["period_from"])
-                      .format("DD.MM.YYYY")}
-                    -
-                    {dayjs
-                      .unix(statisticItem["period_to"])
-                      .format("DD.MM.YYYY")}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={3}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "center",
-                      fontSize: { md: "18px", xs: "12px" },
-                      fontWeight: 400,
-                    }}
-                  >
-                    {statisticItem["visitors"]["views"]}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={2}
-                  sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
-                >
-                  <Typography
-                    sx={{
-                      textAlign: "right",
-                      fontSize: { md: "18px", xs: "12px" },
-                      ml: 1,
-                      fontWeight: 400,
-                    }}
-                  >
-                    {(parseInt(statisticItem["visitors"]["views"]) / 1000) * 30}
-                  </Typography>
-                </Grid>
+                  {groupInfo["name"]}
+                </Typography>
               </Grid>
-            );
-          }
-          return null; // или можно просто ничего не возвращать
+              <Grid
+                item
+                xs={4}
+                sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+              >
+                <Typography
+                  sx={{
+                    textAlign: "center",
+                    fontSize: { md: "18px", xs: "12px" },
+                    fontWeight: 400,
+                  }}
+                >
+                  {date}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+              >
+                <Typography
+                  sx={{
+                    textAlign: "center",
+                    fontSize: { md: "18px", xs: "12px" },
+                    fontWeight: 400,
+                  }}
+                >
+                  {views}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={2}
+                sx={{ borderTop: "1px solid #CDCDCD", pt: 1, pb: 1 }}
+              >
+                <Typography
+                  sx={{
+                    textAlign: "right",
+                    fontSize: { md: "18px", xs: "12px" },
+                    ml: 1,
+                    fontWeight: 400,
+                  }}
+                >
+                  {(parseInt(views) / 1000) * 30}
+                </Typography>
+              </Grid>
+            </Grid>
+          );
         })}
       </Grid>
     )
