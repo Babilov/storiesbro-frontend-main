32d161930d8d345fecda0ded7918d649ed1e00b2
babilovv <egor-kun2017@mail.ru>
2025-06-19 09:32:25 +0300

    1

--- src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/Table.jsx
+++ src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/Table.jsx
@@ -6,6 +6,8 @@ import {
   Box,
   Avatar,
   CircularProgress,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -14,12 +16,15 @@ import DeletePublicModal from "./modals/DeletePublicModal";
 import { API_URL } from "../../../../../constants/constatns";
 
 const Table = ({ publics, setPublics }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   const [statuses, setStatuses] = useState({});
   const [loadingStates, setLoadingStates] = useState({});
   const [deletePublic, setDeletePublic] = useState(false);
   const [publicObj, setPublicObj] = useState(null);
   const [count, setCount] = useState(1);
-  const [isLoading, setIsLoading] = useState(true); // Добавляем общее состояние загрузки
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleDelete = (id) => {
     setPublicObj(publics.filter((item) => item["id"] === id)[0]);
@@ -30,17 +35,15 @@ const Table = ({ publics, setPublics }) => {
 
   useEffect(() => {
     const fetchStatuses = async () => {
-      setIsLoading(true); // Устанавливаем загрузку при начале запроса
+      setIsLoading(true);
       const newStatuses = {};
       const newLoadingStates = {};
 
-      // Инициализируем состояния загрузки
       publics.forEach((obj) => {
         newLoadingStates[obj.group_id] = true;
       });
       setLoadingStates(newLoadingStates);
 
-      // Параллельно запрашиваем все статусы
       const requests = publics.map((obj) =>
         axios
           .get(`${API_URL}community_switch/${obj.group_id}`)
@@ -50,7 +53,7 @@ const Table = ({ publics, setPublics }) => {
           }))
           .catch(() => ({
             id: obj.group_id,
-            status: 0, // В случае ошибки считаем сообщество отключенным
+            status: 0,
           }))
       );
 
@@ -66,14 +69,14 @@ const Table = ({ publics, setPublics }) => {
       } catch (error) {
         console.error("Error fetching statuses:", error);
       } finally {
-        setIsLoading(false); // Выключаем загрузку после завершения запросов
+        setIsLoading(false);
       }
     };
 
     if (publics.length > 0) {
       fetchStatuses();
     } else {
-      setIsLoading(false); // Если нет данных для загрузки, выключаем спиннер
+      setIsLoading(false);
     }
   }, [publics]);
 
@@ -98,7 +101,6 @@ const Table = ({ publics, setPublics }) => {
     );
   };
 
-  // Добавляем общий спиннер при загрузке
   if (isLoading) {
     return (
       <Box
@@ -124,122 +126,62 @@ const Table = ({ publics, setPublics }) => {
         publicObj={publicObj}
       />
 
-      {/* Десктопная версия */}
-      <Box
-        sx={{
-          border: "1px solid #CDCDCD",
-          borderRadius: "10px",
-          p: "0 25px",
-          display: { xs: "none", md: "block" },
-        }}
-      >
-        <Grid
-          container
+      {!isMobile ? (
+        // Десктопная версия
+        <Box
           sx={{
-            display: "flex",
-            textAlign: "center",
-            justifyContent: "center",
-            p: 2,
+            border: "1px solid #CDCDCD",
+            borderRadius: "10px",
+            p: "0 25px",
           }}
-          className="adminTitles"
         >
-          <Grid item xs={3}>
-            <Typography variant="h4">Сообщество</Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="h4">Название</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography variant="h4">Статус</Typography>
-          </Grid>
-          <Grid item xs={4}></Grid>
-        </Grid>
-        <Divider />
-        {publics.map((publicObj) => (
           <Grid
             container
-            key={publicObj.group_id}
-            className="adminPublics"
             sx={{
               display: "flex",
-              alignItems: "center",
-              borderBottom: "1px solid #CDCDCD",
-              pt: 1,
-              pb: 1,
+              textAlign: "center",
+              justifyContent: "center",
+              p: 2,
             }}
+            className="adminTitles"
           >
+            <Grid item xs={3}>
+              <Typography variant="h4">Сообщество</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h4">Название</Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="h4">Статус</Typography>
+            </Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
+          <Divider />
+          {publics.map((publicObj) => (
             <Grid
-              item
-              xs={3}
-              sx={{ display: "flex", justifyContent: "center" }}
-            >
-              <Avatar
-                alt="group avatar"
-                src={publicObj.photo}
-                sx={{ borderRadius: "50%", height: "90px", width: "90px" }}
-              />
-            </Grid>
-            <Grid item xs={3} sx={{ textAlign: "center" }}>
-              <Link
-                to={publicObj.link}
-                style={{
-                  textAlign: "center",
-                  color: "black",
-                  textDecoration: "none",
-                  cursor: "pointer",
-                  fontSize: "18px",
-                  target: "_blank",
-                }}
-              >
-                {publicObj.name}
-              </Link>
-            </Grid>
-            <Grid item md={2}>
-              {renderStatus(publicObj.group_id)}
-            </Grid>
-            <Grid
-              item
-              xs={4}
+              container
+              key={publicObj.group_id}
+              className="adminPublics"
               sx={{
                 display: "flex",
                 alignItems: "center",
-                justifyContent: "center",
+                borderBottom: "1px solid #CDCDCD",
+                pt: 1,
+                pb: 1,
               }}
             >
-              <Link
-                to={`/publics/setting/${publicObj.group_id}`}
-                sx={{ m: 2, cursor: "pointer" }}
-                className="linkItem"
+              <Grid
+                item
+                xs={3}
+                sx={{ display: "flex", justifyContent: "center" }}
               >
-                Настройки
-              </Link>
-            </Grid>
-          </Grid>
-        ))}
-      </Box>
-
-      {/* Мобильная версия */}
-      <Box sx={{ display: { xs: "block", md: "none" } }}>
-        {publics.map((publicObj) => (
-          <Box className="grayBorder" sx={{ mb: 2 }} key={publicObj.group_id}>
-            <Box
-              sx={{
-                display: "flex",
-                borderBottom: "1px solid #CBCBCB",
-                position: "relative",
-              }}
-            >
-              <Box sx={{ display: "flex" }}>
                 <Avatar
-                  alt="avatar"
+                  alt="group avatar"
                   src={publicObj.photo}
-                  sx={{
-                    m: 2,
-                    borderRadius: "50%",
-                    height: "70px",
-                    width: "70px",
-                  }}
+                  sx={{ borderRadius: "50%", height: "90px", width: "90px" }}
                 />
+              </Grid>
+              <Grid item xs={3} sx={{ textAlign: "center" }}>
                 <Link
                   to={publicObj.link}
                   style={{
@@ -247,42 +189,103 @@ const Table = ({ publics, setPublics }) => {
                     color: "black",
                     textDecoration: "none",
                     cursor: "pointer",
-                    fontSize: "12px",
-                    fontWeight: 600,
-                    marginTop: "16px",
+                    fontSize: "18px",
                     target: "_blank",
                   }}
                 >
                   {publicObj.name}
                 </Link>
-              </Box>
-              {renderStatus(publicObj.group_id)}
-            </Box>
-            <Box className="spaceAround">
-              <Link
-                to={`/publics/setting/${publicObj.group_id}`}
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 400,
-                  textDecoration: "none",
-                  margin: 2,
-                  cursor: "pointer",
-                  color: "black",
+              </Grid>
+              <Grid item md={2}>
+                {renderStatus(publicObj.group_id)}
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                Настройки
-              </Link>
-              <Typography sx={{ color: "#CBCBCB", m: 2 }}>|</Typography>
-              <Typography
-                sx={{ fontSize: "12px", m: 2, cursor: "pointer" }}
-                onClick={() => handleDelete(publicObj.group_id)}
+                <Link
+                  to={`/publics/setting/${publicObj.group_id}`}
+                  sx={{ m: 2, cursor: "pointer" }}
+                  className="linkItem"
+                >
+                  Настройки
+                </Link>
+              </Grid>
+            </Grid>
+          ))}
+        </Box>
+      ) : (
+        // Мобильная версия
+        <Box>
+          {publics.map((publicObj) => (
+            <Box className="grayBorder" sx={{ mb: 2 }} key={publicObj.group_id}>
+              <Box
+                sx={{
+                  display: "flex",
+                  borderBottom: "1px solid #CBCBCB",
+                  position: "relative",
+                }}
               >
-                Отключить
-              </Typography>
+                <Box sx={{ display: "flex" }}>
+                  <Avatar
+                    alt="avatar"
+                    src={publicObj.photo}
+                    sx={{
+                      m: 2,
+                      borderRadius: "50%",
+                      height: "70px",
+                      width: "70px",
+                    }}
+                  />
+                  <Link
+                    to={publicObj.link}
+                    style={{
+                      textAlign: "center",
+                      color: "black",
+                      textDecoration: "none",
+                      cursor: "pointer",
+                      fontSize: "12px",
+                      fontWeight: 600,
+                      marginTop: "16px",
+                      target: "_blank",
+                    }}
+                  >
+                    {publicObj.name}
+                  </Link>
+                </Box>
+                {renderStatus(publicObj.group_id)}
+              </Box>
+              <Box className="spaceAround">
+                <Link
+                  to={`/publics/setting/${publicObj.group_id}`}
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: 400,
+                    textDecoration: "none",
+                    margin: 2,
+                    cursor: "pointer",
+                    color: "black",
+                  }}
+                >
+                  Настройки
+                </Link>
+                <Typography sx={{ color: "#CBCBCB", m: 2 }}>|</Typography>
+                <Typography
+                  sx={{ fontSize: "12px", m: 2, cursor: "pointer" }}
+                  onClick={() => handleDelete(publicObj.group_id)}
+                >
+                  Отключить
+                </Typography>
+              </Box>
             </Box>
-          </Box>
-        ))}
-      </Box>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
