14513a43b65f3ebb81dbf7b02a7dfae937f9156a
Babilov <egor-kun2017@mail.ru>
2025-10-18 10:01:06 +0300

    1

--- src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/modals/AddPublicModal.jsx
+++ src/components/AuthorizedAdmin/creatives/creativesAfterEnter/table/modals/AddPublicModal.jsx
@@ -7,6 +7,7 @@ import NoPermissionModal from "./NoPermissionModal";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../../../../../../constants/constatns";
+import { fetchWithAuth } from "../../../../../../api/token";
 
 const AddPublicModal = ({ open, setOpen, publics, addedPublics }) => {
   const [error, setError] = useState(false);
@@ -135,7 +136,7 @@ const AddPublicModal = ({ open, setOpen, publics, addedPublics }) => {
   };
 
   const addAcc = async () => {
-    const res = await axios.get(`${API_URL}communities/vk/link-account/`);
+    const res = await fetchWithAuth(`${API_URL}communities/vk/link-account/`);
     console.log(res);
   };
 
