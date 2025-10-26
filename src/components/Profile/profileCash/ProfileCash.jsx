941ab563227325c7ea51f7c89dbe489eafb34d5a
Babilov <egor-kun2017@mail.ru>
2025-10-26 17:50:14 +0300

    1

--- src/components/Profile/profileCash/ProfileCash.jsx
+++ src/components/Profile/profileCash/ProfileCash.jsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import MyInput from "../../UI/input/MyInput";
 import ProfileCashButton from "./ProfileCashButton";
 import { useEffect, useState } from "react";
 import { fetchWithAuth } from "../../../api/token";
@@ -7,6 +6,7 @@ import { API_URL } from "../../../constants/constatns";
 
 const ProfileCash = () => {
   const [balance, setBalance] = useState(null);
+
   useEffect(() => {
     const getMoney = async () => {
       const res = await fetchWithAuth(`${API_URL}payments/get_user_balance/`);
@@ -28,9 +28,25 @@ const ProfileCash = () => {
         <Typography sx={{ fontWeight: 600, fontSize: "18px", mr: 2 }}>
           Баланс
         </Typography>
-        <MyInput value={balance} disabled={true} />
+        <Box
+          sx={{
+            px: 2,
+            py: 1,
+            border: "1px solid #ccc",
+            borderRadius: "8px",
+            minWidth: "100px",
+            textAlign: "center",
+            fontWeight: 500,
+            fontSize: "16px",
+            mr: 1,
+            backgroundColor: "#f9f9f9",
+          }}
+        >
+          {balance !== null ? balance : "—"}
+        </Box>
         <Typography sx={{ fontWeight: 600, fontSize: "20px" }}>₽</Typography>
       </Box>
+
       <Box
         sx={{
           m: { lg: "0 160px 0 0", xs: "0 auto" },
@@ -38,12 +54,6 @@ const ProfileCash = () => {
           width: { lg: "50%", md: "50%", xs: "100%" },
         }}
       >
-        {/* <ProfileCashButton
-          to="/cash/deposit"
-          text="Пополнить"
-          background="#E68B46"
-          color="white"
-        /> */}
         <ProfileCashButton
           to="/cash/get-money"
           text="Вывести"
