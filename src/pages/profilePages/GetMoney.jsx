c03d8696fbc3aa210d1bd7a3240bb6645c195331
Babilov <egor-kun2017@mail.ru>
2025-10-27 22:01:09 +0300

    1

--- src/pages/profilePages/GetMoney.jsx
+++ src/pages/profilePages/GetMoney.jsx
@@ -1,6 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
-
 import GetMoneyTable from "../../components/Profile/profileCash/getMoney/GetMoneyTable";
 import CashError from "../../components/Profile/profileCash/CashModals/CashError";
 import { CashContext } from "../../components/Profile/profileCash/CashContext";
@@ -11,21 +10,10 @@ import { MY_URL } from "../../constants/constatns";
 const GetMoney = () => {
   const DEPOSIT_URL = `${MY_URL}api/payments/conclusions/`;
 
-  useEffect(() => {
-    const getDeposit = async () => {
-      try {
-        const res = await fetchWithAuth(DEPOSIT_URL);
-        setOperations((prev) => [...res.results, ...prev]);
-      } catch {
-        console.log("error");
-      }
-    };
-    getDeposit();
-  }, []);
-
   const [
     ,
     ,
+    // некоторые значения не используются
     errorModalOpen,
     setErrorModalOpen,
     codeModal,
@@ -34,12 +22,48 @@ const GetMoney = () => {
     setOperations,
   ] = useContext(CashContext);
 
+  const [loading, setLoading] = useState(true); // флаг загрузки
+
+  useEffect(() => {
+    const getDeposit = async () => {
+      try {
+        const res = await fetchWithAuth(DEPOSIT_URL);
+        setOperations((prev) => [...res.results, ...prev]);
+      } catch {
+        console.log("error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getDeposit();
+  }, []);
+
   return (
-    <Box>
+    <Box sx={{ position: "relative", minHeight: "200px" }}>
       <CashError open={errorModalOpen} setOpen={setErrorModalOpen} />
       {/*<CashConfirmationModal open={codeModal} setOpen={setCodeModal} /> */}
-      <GetMoneyTable operations={operations} />
-      <GetMoneyMobileTable operations={operations} />
+
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "300px",
+          }}
+        >
+          <CircularProgress
+            size={60}
+            thickness={4.5}
+            sx={{ color: "orange" }}
+          />
+        </Box>
+      ) : (
+        <>
+          <GetMoneyTable operations={operations} />
+          <GetMoneyMobileTable operations={operations} />
+        </>
+      )}
     </Box>
   );
 };
